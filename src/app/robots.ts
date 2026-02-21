e6bfdec871b44b9057dee87aadeb7952b8f54b59
Shaishav Shah <shaishav.shah04@gmail.com>
2026-02-21 14:56:25 -0500

    block GPTBot and bytespider in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,11 +5,21 @@ export default function robots(): MetadataRoute.Robots {
         process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 
     return {
-        rules: {
-            userAgent: "*",
-            allow: ["/"],
-            disallow: ["/api/", "/outstatic/"],
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: ["/"],
+                disallow: ["/api/", "/outstatic/"],
+            },
+            {
+                userAgent: "GPTBot",
+                disallow: ["/"],
+            },
+            {
+                userAgent: "bytespider",
+                disallow: ["/"],
+            },
+        ],
         sitemap: `${baseUrl}/sitemap.xml`,
     };
 }
